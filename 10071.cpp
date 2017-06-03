1baf5b31308751feedef97971aafec30a2bae31c
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 16:18:16 +0600

    Create 10071.cpp

---
+++ 10071.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdio>
+#include<math.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int a,t,d;
+    while(scanf("%d%d",&a,&t) !=EOF){
+        d = a*t;
+        cout<<d*2<<endl;
+    }
+
+    return 0;
+}
