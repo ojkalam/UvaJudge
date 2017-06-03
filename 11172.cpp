1ad7d4c3d9dfedeee3992e81ad4d9a4f14690ad3
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 16:18:43 +0600

    Create 11172.cpp

---
+++ 11172.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+#include<math.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        if(a>b){
+            cout<<">"<<endl;
+        }
+        else if(a<b){
+            cout<<"<"<<endl;
+        }
+        else if(a==b){
+            cout<<"="<<endl;
+        }
+    }
+    return 0;
+}
